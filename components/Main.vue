8e0a181940e5e2a32dca94fd43e8703d834c9426
romario-giacholari <giacholari@gmail.com>
2022-10-15 18:20:37 +0100

    replicate giacholari.com

--- components/Main.vue
+++ components/Main.vue
@@ -1,9 +1,31 @@
 <template>
-    <div id="app" style="min-height: 100vh;" class="mt-4 p-4">
-    <div class="columns is-mobile is-centered">
-        <span class="loader"></span>
+  <div id="app" style="min-height: 100vh;" class="mt-4 p-4">
+    <div class="container is-max-desktop">
+      <div class="block">
+        <span class="title is-4">ROMARIO GIACHOLARI</span>
+      </div>
+
+      <hr />
+
+      <div>
+        Hello and welcome to the site. I am a software engineer and computer science graduate from <a href="https://www.aston.ac.uk/" class="underline" title="aston university">aston university</a>.
+        I enjoy tackling problems and discussing about software. 
+        It is amazing what it can do and at what scale it enables us to reach out to people.
+        When not programming, I attempt to keep myself active with 5-aside football games.
+        If you want to get in touch, send me an email <a href="mailto:giacholari@gmail.com" class="underline" title="giacholari@gmail.com">giacholari@gmail.com</a>.
+        To find out more about my experience and the specific technologies I work with, see my <a href="https://giacholari.com/static/pdf/resume.pdf" class="underline" title="resume">resume</a>.
+      </div>
+
+      <div class="mt-2">
+        <ul>
+          <li><a href="https://tillo.io/" class="underline" title="tillo">Tillo</a> - Software Engineer 2020/2022</li>
+          <li><a href="https://www.pinewood.co.uk/" class="underline" title="pinewood technologies">Pinewood Technologies</a> - Software Engineer Intern (placement) 2018/2020 </li>
+          <li><a href="https://www.bootcampmedia.co.uk/" class="underline" title="bootcampmedia">Bootcampmedia</a> - Software Engineer (part time) 2018</li>
+          <li><a href="https://beautifulcanoe.com/" class="underline" title="beautiful canoe">Beautiful Canoe</a> - Software Engineer Intern 2017</li>
+        </ul>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
